3c74173af3ccd585ad7919c536fe527a27a23388
ALEJANDRO <ALEJANDRO@DESKTOP-293SJF1>
2019-09-30 09:09:56 -0500

    Cree un directorio bien papi

---
+++ src/directorio_mejia/Directorio_Mejia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package directorio_mejia;
+
+/**
+ *
+ * @author ALEJANDRO
+ */
+public class Directorio_Mejia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
